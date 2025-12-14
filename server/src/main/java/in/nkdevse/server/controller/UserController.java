e4e4ba15dda66c4ec3f26fdd13722142140f4a3f
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2025-12-14 11:41:06 +0530

    modify logo jsx controller
    modify logo.png for index.html
    testimonials for key prop
    modify for user controller

--- server/src/main/java/in/nkdevse/server/controller/UserController.java
+++ server/src/main/java/in/nkdevse/server/controller/UserController.java
@@ -5,6 +5,7 @@ import in.nkdevse.server.response.RemoveBgResponse;
 import in.nkdevse.server.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,29 +20,37 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public RemoveBgResponse createOrUpdateUser(@RequestBody UserDto userDTO, Authentication authentication) {
+    public ResponseEntity<?> createOrUpdateUser(@RequestBody UserDto userDTO, Authentication authentication) {
+
+        RemoveBgResponse response;
 
         try {
             if (!authentication.getName().equals(userDTO.getClerkId())) {
-                return RemoveBgResponse.builder()
+                response = RemoveBgResponse.builder()
                         .success(false)
-                        .data("User Unauthorized")
+                        .data("User does not have permission to access the resource")
                         .statusCode(HttpStatus.FORBIDDEN)
                         .build();
+
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(response);
             }
 
             UserDto user = userService.saveUser(userDTO);
-            return RemoveBgResponse.builder()
+            response = RemoveBgResponse.builder()
                     .success(true)
                     .data(user)
-                    .statusCode(HttpStatus.CREATED)
+                    .statusCode(HttpStatus.OK)
                     .build();
+
+            return ResponseEntity.status(HttpStatus.OK).body(response);
         } catch (Exception exception) {
-            return RemoveBgResponse.builder()
+            response = RemoveBgResponse.builder()
                     .success(true)
                     .data(exception.getMessage())
                     .statusCode(HttpStatus.INTERNAL_SERVER_ERROR)
                     .build();
+
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
         }
 
 
